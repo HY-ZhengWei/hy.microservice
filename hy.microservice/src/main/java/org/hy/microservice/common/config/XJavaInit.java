3249b5b54df3410ee786ace2aae3e827eb0e10f8
ZhengWei <HY.ZhengWei@qq.com>
2023-05-29 16:01:24 +0800

    1.0.0
    1. 优化：启动配置的加载方式和启动顺序，方便子项目的继承和引用

--- hy.microservice/src/main/java/org/hy/microservice/common/config/XJavaInit.java
+++ hy.microservice/src/main/java/org/hy/microservice/common/config/XJavaInit.java
@@ -60,14 +60,24 @@ public class XJavaInit extends AppInitConfig
             
             try
             {
-                this.init("config/ms.sys.Config.xml"     ,this.xmlRoot);
-                this.init("config/ms.startup.Config.xml" ,this.xmlRoot);
-                this.init((List<Param>)XJava.getObject("StartupConfig_MS_Common") ,this.xmlRoot);
-                this.init(((Param)XJava.getObject("MS_Common_RootPackageName")).getValue());
+                this.loadXML("config/ms.sys.Config.xml" ,this.xmlRoot);
+                
+                // 系统参数加载之后，公共资源加载之前，先行加载子项目的初始化参数
+                this.loadDirectory("config/initialization/" ,this.xmlRoot);
+                
+                // 加载子项目的启动配置
+                this.loadDirectory("config/startup/" ,this.xmlRoot);
+                
+                this.loadXML("config/ms.startup.Config.xml" ,this.xmlRoot);
+                this.loadXML((List<Param>)XJava.getObject("StartupConfig_MS_Common") ,this.xmlRoot);
+                this.loadClasses(((Param)XJava.getObject("MS_Common_RootPackageName")).getValue());
+                
+                // 在注解解释完成后，在Job任务解释之前执行
+                this.loadDirectory("config/startupFinish/" ,this.xmlRoot);
                 
                 if ( i_IsStartJobs )
                 {
-                    this.init("config/ms.job.xml" ,this.xmlRoot);
+                    this.loadXML("config/ms.job.xml" ,this.xmlRoot);
                 }
             }
             catch (Exception exce)
