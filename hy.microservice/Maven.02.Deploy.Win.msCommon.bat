9d1c4d55fd8d8ce909b86f050a591b172b2e24ad
ZhengWei <HY.ZhengWei@qq.com>
2023-06-12 09:38:18 +0800

    1.0.9
    1.0.8发布失败，只能用新的版本号发布

--- hy.microservice/Maven.02.Deploy.Win.msCommon.bat
+++ hy.microservice/Maven.02.Deploy.Win.msCommon.bat
@@ -1,8 +1,8 @@
-call mvn install:install-file -Dfile=./target/hy.microservice-1.0.8.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
-call mvn install:install-file -Dfile=./target/hy.microservice-1.0.8-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml
+call mvn install:install-file -Dfile=./target/hy.microservice-1.0.9.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
+call mvn install:install-file -Dfile=./target/hy.microservice-1.0.9-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml
 
 
-call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.0.8.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.0.8-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.0.9.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.0.9-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
 
 pause
