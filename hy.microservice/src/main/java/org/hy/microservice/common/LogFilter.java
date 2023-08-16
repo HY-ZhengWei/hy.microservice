655426b42f4170cefe74e0f527ec476ec8c31272
HY-ZhengWei <HY.ZhengWei@qq.com>
2023-08-16 21:50:30 +0800

    1.2.6
    1. 添加：Web请求接口 @XRequest 的事件监监听器接口，支持它接口的日志、黑白名单等功能
    2. 修正：小概率未记录 "访问量达到上限" 的日志

--- hy.microservice/src/main/java/org/hy/microservice/common/LogFilter.java
+++ hy.microservice/src/main/java/org/hy/microservice/common/LogFilter.java
@@ -19,11 +19,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.hy.common.Date;
 import org.hy.common.Help;
+import org.hy.common.Return;
 import org.hy.common.StringHelp;
 import org.hy.common.TimeGroupTotal;
 import org.hy.common.xml.XJSON;
 import org.hy.common.xml.XJava;
+import org.hy.common.xml.event.XRequestListener;
 import org.hy.common.xml.log.Logger;
+import org.hy.common.xml.plugins.AppInterfaces;
+import org.hy.common.xml.plugins.AppMessage;
 import org.hy.common.xml.plugins.XSQLFilter;
 import org.hy.microservice.common.ipSafe.IIPSafeConfigService;
 import org.hy.microservice.common.ipSafe.IPSafeConfig;
@@ -43,13 +47,15 @@ import org.hy.microservice.common.operationLog.OperationLog;
  *              v2.0  2023-08-09  添加：接口模块编号和URL地址，允许更细粒度的控制黑白名单
  *                                添加：黑白名单的命中率功能，提高黑白名单的判定性能
  *              v3.0  2023-08-11  添加：延时单线程队列周期性的处理日志的持久化
+ *              v4.0  2023-08-16  添加：Web请求接口 @XRequest 的事件监监听器接口，支持它接口的日志、黑白名单等功能
+ *                                修正：小概率未记录 "访问量达到上限" 的日志
  */
 @WebFilter(filterName="logFilter" ,urlPatterns="/*" ,initParams={
         @WebInitParam(name="exclusions" ,value="*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.swf")
        ,@WebInitParam(name="cachesize"  ,value="1000")
        ,@WebInitParam(name="timeout"    ,value="60")
 })
-public class LogFilter extends XSQLFilter
+public class LogFilter extends XSQLFilter implements XRequestListener
 {
     private static final Logger                      $Logger           = new Logger(LogFilter.class);
     
@@ -80,6 +86,8 @@ public class LogFilter extends XSQLFilter
         this.apiUseMaxCountMinute   = Long.valueOf(XJava.getParam("MS_Common_ApiUseMaxCountMinute").getValue());
         this.apiUseMaxCountMinute10 = Long.valueOf(XJava.getParam("MS_Common_ApiUseMaxCountMinute10").getValue());
         this.systemCode             = XJava.getParam("MS_Common_ServiceName").getValue();
+        
+        AppInterfaces.setListener(this);
     }
     
     
@@ -311,6 +319,7 @@ public class LogFilter extends XSQLFilter
     {
         String    v_Url  = ((HttpServletRequest) i_ServletRequest).getServletPath();
         String [] v_Urls = v_Url.split("/");
+        
         if ( StringHelp.isContains(v_Url ,"analyse" ,".") || v_Urls.length < 3 )
         {
             i_FilterChain.doFilter(i_ServletRequest ,i_ServletResponse);
@@ -343,7 +352,7 @@ public class LogFilter extends XSQLFilter
             v_OLog.setSystemCode(this.systemCode);
             v_OLog.setModuleCode(v_Urls[1]);
             
-            this.operationLogService.insert(this.backWhiteCheck(v_OLog));
+            this.backWhiteCheck(v_OLog);
         }
         catch (Exception exce)
         {
@@ -360,31 +369,35 @@ public class LogFilter extends XSQLFilter
             i_ServletResponse.setContentType("application/json");
             v_Output = i_ServletResponse.getOutputStream();
             v_Output.write(v_OLog.getUrlResponse().getBytes());
+            this.operationLogService.insert(v_OLog);
             return;
         }
         // 访问量达到上限
-        if ( !this.allowUseAPIMinute(v_OLog.getUrl()) )
+        else if ( !this.allowUseAPIMinute(v_OLog.getUrl()) )
         {
             v_OLog.setUrlResponse("访问量达到上限");
             i_ServletResponse.setCharacterEncoding("UTF-8");
             i_ServletResponse.setContentType("application/json");
             v_Output = i_ServletResponse.getOutputStream();
             v_Output.write(v_OLog.getUrlResponse().getBytes());
+            this.operationLogService.insert(v_OLog);
             return;
         }
         // 访问量达到上限（10分钟）
-        if ( !this.allowUseAPIMinute10(v_OLog.getUrl()) )
+        else if ( !this.allowUseAPIMinute10(v_OLog.getUrl()) )
         {
             v_OLog.setUrlResponse("访问量达到10分钟上限");
             i_ServletResponse.setCharacterEncoding("UTF-8");
             i_ServletResponse.setContentType("application/json");
             v_Output = i_ServletResponse.getOutputStream();
             v_Output.write(v_OLog.getUrlResponse().getBytes());
+            this.operationLogService.insert(v_OLog);
             return;
         }
         
+        this.operationLogService.insert(v_OLog);
         super.doFilter(v_Request ,v_Response ,i_FilterChain);
-        //i_FilterChain.doFilter(v_Request ,v_Response);
+        // 原始的方式 i_FilterChain.doFilter(v_Request ,v_Response);
         
         try
         {
@@ -432,6 +445,105 @@ public class LogFilter extends XSQLFilter
             v_Output = null;
         }
     }
+    
+
+	
+	/**
+     * 在执行前一刻被触发。
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2023-08-16
+     * @version     v1.0
+     *
+     * @param i_Request      请求对象
+     * @param i_Response     响应对象
+     * @param io_RequestData 请求数据
+     * @param i_Message      请求数据的原始报文
+	 * @return               当返回 false 时，中断 "执行"。
+	 *                       当返回 true  时，执行接口调用
+	 */
+	public Return<Object> before(HttpServletRequest  i_Request
+			                    ,HttpServletResponse i_Response 
+			                    ,AppMessage<?>       io_RequestData 
+			                    ,String              i_Message)
+	{
+		Return<Object> v_Ret  = new Return<Object>(true);
+		OperationLog   v_OLog = new OperationLog();
+		
+		v_OLog.setCreateTime(new Date());
+        v_OLog.setId(StringHelp.getUUID());
+        v_OLog.setUrl("/app/" + io_RequestData.getSid());
+        v_OLog.setUrlRequest(i_Request.getQueryString());
+        v_OLog.setUrlRequestBody(i_Message);
+        v_OLog.setUserIP(getIpAddress(i_Request));
+        v_OLog.setSystemCode(this.systemCode);
+        v_OLog.setModuleCode("/app");
+		
+        this.backWhiteCheck(v_OLog);
+        
+        // 阻止访问
+        if ( !Help.isNull(v_OLog.getUrlResponse()) )
+        {
+            v_Ret.set(false).setParamStr(v_OLog.getUrlResponse());
+        }
+	    // 访问量达到上限
+        else if ( !this.allowUseAPIMinute(v_OLog.getUrl()) )
+        {
+            v_OLog.setUrlResponse("访问量达到上限");
+            v_Ret.set(false).setParamStr(v_OLog.getUrlResponse());
+        }
+        // 访问量达到上限（10分钟）
+        else if ( !this.allowUseAPIMinute10(v_OLog.getUrl()) )
+        {
+            v_OLog.setUrlResponse("访问量达到10分钟上限");
+            v_Ret.set(false).setParamStr(v_OLog.getUrlResponse());
+        }
+        
+        this.operationLogService.insert(v_OLog);
+		return v_Ret;
+	}
+	
+	
+	
+	/**
+     * 执行成功后被触发。
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2023-08-16
+     * @version     v1.0
+     *
+     * @param i_RequestData   请求数据
+     * @param i_ResponseData  响应数据
+     * @param i_Other         用户自定义的数据
+	 */
+	public void succeed(AppMessage<?> i_RequestData ,AppMessage<?> i_ResponseData ,Object i_Other)
+	{
+		OperationLog v_OLog = (OperationLog)i_Other;
+		
+		v_OLog.setResultCode(BaseResponse.$Succeed);
+		this.operationLogService.update(v_OLog);
+	}
+	
+	
+	
+	/**
+     * 执行异常后被触发。
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2023-08-16
+     * @version     v1.0
+     *
+     * @param i_RequestData   请求数据
+     * @param i_Exception     异常数据
+     * @param i_Other         用户自定义的数据
+	 */
+	public void fail(AppMessage<?> i_RequestData ,Exception i_Exception ,Object i_Other)
+	{
+		OperationLog v_OLog = (OperationLog)i_Other;
+		
+		v_OLog.setResultCode(i_RequestData.getRc());
+		this.operationLogService.update(v_OLog);
+	}
 
 
 
