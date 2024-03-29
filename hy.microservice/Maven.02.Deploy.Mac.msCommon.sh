ce00fa9059b29071d266ec1ecc9f42420a7620b4
ZhengWei <HY.ZhengWei@qq.com>
2024-03-29 13:37:31 +0800

    1.7.4
    1. 修正：XSQL组嵌套，A为父，B为子，当Java调用XSQL组时跳过A，直接调用B时，会造成数据库连接未释放的问题

--- hy.microservice/Maven.02.Deploy.Mac.msCommon.sh
+++ hy.microservice/Maven.02.Deploy.Mac.msCommon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=./target/hy.microservice-1.7.3.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
-mvn install:install-file -Dfile=./target/hy.microservice-1.7.3-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml
+mvn install:install-file -Dfile=./target/hy.microservice-1.7.4.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
+mvn install:install-file -Dfile=./target/hy.microservice-1.7.4-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml
 
 
-mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.7.3.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.7.3-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.7.4.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.7.4-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
