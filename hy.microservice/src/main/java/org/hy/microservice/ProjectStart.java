4516f1ce32c4f6bb2a49dea489b3a33a031d615a
ZhengWei <HY.ZhengWei@qq.com>
2024-06-25 20:53:37 +0800

    1.8.4
    1. 添加：SpringBoot 动态扫描包路径

--- hy.microservice/src/main/java/org/hy/microservice/ProjectStart.java
+++ hy.microservice/src/main/java/org/hy/microservice/ProjectStart.java
@@ -18,7 +18,6 @@ import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -40,7 +39,6 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
  *              v2.0  2021-02-19  添加：支持SpringBoot 2.4.0版本
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
-@ServletComponentScan
 public class ProjectStart extends SpringBootServletInitializer
 {
     public static final Map<String ,OperationLogModule>       $RequestMappingModules = new HashMap<String ,OperationLogModule>();
