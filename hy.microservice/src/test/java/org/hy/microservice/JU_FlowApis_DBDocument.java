a335b18865695d3c2e0e524eb914a6cc17cda466
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-01-21 19:21:24 +0800

    1.9.2
    1. 修正：日志输出的更详细

---
+++ hy.microservice/src/test/java/org/hy/microservice/JU_FlowApis_DBDocument.java
@@ -0,0 +1,45 @@
+package org.hy.microservice;
+
+import org.hy.microservice.common.config.XJavaInit;
+import org.junit.Test;
+
+
+
+
+
+/**
+ * 数据库设计文档的生成
+ *
+ * @author      ZhengWei(HY)
+ * @createDate  2025-01-07
+ * @version     v1.0
+ */
+public class JU_FlowApis_DBDocument
+{
+    
+    private static boolean $IsInit = false;
+    
+    
+    
+    public JU_FlowApis_DBDocument()
+    {
+        synchronized ( this )
+        {
+            if ( !$IsInit )
+            {
+                $IsInit = true;
+                
+                new XJavaInit(false ,"D:\\Software\\apache-tomcat-9.0.96\\webapps\\lps.microservice.flowapis\\WEB-INF\\classes\\");
+            }
+        }
+    }
+    
+    
+    
+    @Test
+    public void makeDoc()
+    {
+        DBDocument.makeDatabaseDoc("DS_MS_FlowApis" ,"V1.0" ,null ,"OpenApi.数据库设计-FlowApis柔性编排");
+    }
+    
+}
