26d505c5b2d37978cfbea3e95eaa09f5da2f7372
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-01-23 14:37:19 +0800

    1.9.5
    1.添加：Modbus组件

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.9.4\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.9.5\WEB-INF\classes\* .\target\classes
