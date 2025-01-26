38653a14a9e1e46aff4e5b4983acf5d35503f85e
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-01-26 16:20:44 +0800

    1.9.6
    1. 添加：HART组件

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.9.5\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.9.6\WEB-INF\classes\* .\target\classes
