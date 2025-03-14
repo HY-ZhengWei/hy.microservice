c8adcfddebad9f52e3bf708f4a2f786b686a3871
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-03-14 08:28:23 +0800

    1.10.8
    1.升级：编排引擎

--- hy.microservice/Maven.02.Deploy.Win.msCommon.bat
+++ hy.microservice/Maven.02.Deploy.Win.msCommon.bat
@@ -1,8 +1,8 @@
-call mvn install:install-file -Dfile=./target/hy.microservice-1.10.2.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
-call mvn install:install-file -Dfile=./target/hy.microservice-1.10.2-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api
+call mvn install:install-file -Dfile=./target/hy.microservice-1.10.8.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
+call mvn install:install-file -Dfile=./target/hy.microservice-1.10.8-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api
 
 
-call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.10.2.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml                      -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.10.2-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.10.8.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml                      -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+call mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.10.8-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
 
 pause
