33099f33e2527eb40bec8ab721b288ce0516b8ad
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-05-07 18:05:02 +0800

    1.11.9
    1. 添加：编排添加MQTT类型的订阅元素

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.11.8\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.11.9\WEB-INF\classes\* .\target\classes
