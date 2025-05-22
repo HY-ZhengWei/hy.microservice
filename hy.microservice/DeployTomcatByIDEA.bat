80de97992fa3df7fb6f1e5c781ec1580f1893f98
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-05-22 10:51:54 +0800

    1.12.5
    1. 修正：PLC转XML的格式
    2. 修正：条件元素的日志显示格式

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.12.4\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.12.5\WEB-INF\classes\* .\target\classes
