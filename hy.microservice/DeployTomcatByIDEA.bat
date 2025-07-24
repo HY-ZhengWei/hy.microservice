0348a70ff414cfd5a249f6fe8e484929c3e87472
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-07-24 09:32:07 +0800

    1.13.7
    修正：格式化Json字符串时，对占位符的处理上有在冒号与变量中间添加空格。发现人：王雨墨、景浩东

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.13.5\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.13.7\WEB-INF\classes\* .\target\classes
