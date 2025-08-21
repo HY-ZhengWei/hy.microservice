d222b9f48608e9ebbd9bcb8e0b918ba85a2f90e7
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-08-21 10:31:58 +0800

    1.15.5
    1.PLC. 修正：应少了默认值，造成FastJson无法转为Json的问题
    2.PLC. 升级：PLC4X包到0.13.0
    3.编排：修正：静态检查出异常时，未能在上下文记录异常。发现人：李浩

--- hy.microservice/Maven.02.Deploy.Mac.msCommon.sh
+++ hy.microservice/Maven.02.Deploy.Mac.msCommon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=./target/hy.microservice-1.15.3.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
-mvn install:install-file -Dfile=./target/hy.microservice-1.15.3-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api
+mvn install:install-file -Dfile=./target/hy.microservice-1.15.5.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
+mvn install:install-file -Dfile=./target/hy.microservice-1.15.5-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api
 
 
-mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.15.3.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.15.3-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.15.5.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.15.5-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
