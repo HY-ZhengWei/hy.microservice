03a3e5687e7692ae57b881dc51cfafe970ebe548
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-08-28 16:57:19 +0800

    1.15.7
    1. 添加：静态检查规则，For循环元素的必须有下一个成功路由
    2. 添加：静态检测规则 ，For元素禁止自已引用自己，即：禁止For元素递归
    3. 添加：通过路由类型向外界区分普通路由、For循环路由、While循环路由、自循环路由
    4. 添加：Modbus协议在出现异常时可控制是否自动重新连接的功能
    5. 添加：PLC S7和S7-200 Smart协议在出现异常时可控制是否自动重新连接的功能

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.15.5\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.15.7\WEB-INF\classes\* .\target\classes
