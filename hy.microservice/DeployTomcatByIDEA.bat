b0537e48174979fa2519126c577afafd8325a196
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-09-16 14:04:35 +0800

    1.16.6
    1. 添加：ENF密文元素 和 DEF解文元素
    2. 添加：压缩元素
    3. 添加：解压元素

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.16.5\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.16.6\WEB-INF\classes\* .\target\classes
