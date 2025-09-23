af23c7e7ba070445b1003da279be1c3cadfb37bd
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-09-23 08:09:26 +0800

    1.16.9
    1. 添加：蟒蛇元素

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.16.7\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.16.9\WEB-INF\classes\* .\target\classes
