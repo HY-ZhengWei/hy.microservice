058d8a6a25bdde0643b9ed2e1f1f7ea2b6ee6667
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-09-29 08:17:24 +0800

    1.17.3
    1. 添加：SH脚本元素
    2. 添加：多个执行元素的子类，转XML时是否显示retFalseIsError属性
    3. 添加：串口通讯，RTS引脚、DTR引脚、TxD线强制低电平
    4. 修正：HART关闭串口设备的close方法未加同步锁
    5. 优化：串口通讯，设置为连续请求，优化通信
    6. 添加：特性化的静态检查

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.17.2\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.17.3\WEB-INF\classes\* .\target\classes
--- hy.microservice/Maven.02.Deploy.Mac.msCommon.sh
+++ hy.microservice/Maven.02.Deploy.Mac.msCommon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=./target/hy.microservice-1.17.2.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
-mvn install:install-file -Dfile=./target/hy.microservice-1.17.2-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api
+mvn install:install-file -Dfile=./target/hy.microservice-1.17.3.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml
+mvn install:install-file -Dfile=./target/hy.microservice-1.17.3-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api
 
 
-mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.17.2.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.17.2-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.17.3.war     -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom.xml     -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=./target/hy.microservice-1.17.3-api.jar -DpomFile=./src/main/webapp/META-INF/maven/cn.openapis/hy.microservice/pom-api.xml -Dclassifier=api -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
