7fb06b0134caf246d028be822304325eccf074fe
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-11-10 18:31:55 +0800

    1.18.9
    1. 添加：模拟元素
    2. 添加：众多元素一一实现模拟功能

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.18.8\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.18.9\WEB-INF\classes\* .\target\classes
