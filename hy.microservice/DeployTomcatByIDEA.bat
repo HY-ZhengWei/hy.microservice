d02b20c8c3b9c7bbba7955d4d8f9796cee546f07
HY-ZhengWei <HY.ZhengWei@qq.com>
2026-01-13 08:19:18 +0800

    1.19.7
    1. 优化：PLC组件：日志输出逻辑，方便在《日志分析》页面上排查问题
    2. 添加：File组件：最大用时
    3. 添加：XJava组件：最大用时的日志统计

--- hy.microservice/DeployTomcatByIDEA.bat
+++ hy.microservice/DeployTomcatByIDEA.bat
@@ -1,1 +1,1 @@
-mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.19.6\WEB-INF\classes\* .\target\classes
+mvn clean compile package && xcopy /S/Y .\target\hy.microservice-1.19.7\WEB-INF\classes\* .\target\classes
